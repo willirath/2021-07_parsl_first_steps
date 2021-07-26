846b00df7d6d49d143a6485465fea6ee761d8a21
Willi Rath <wrath@geomar.de>
2021-07-26 18:47:28 +0200

    Run

---
+++ 2021.07.26.1_slurm_nesh/cmd_parsl.slurm.1627317929.9002402.sh
@@ -0,0 +1,1 @@
+process_worker_pool.py   -a 172.18.4.13 -p 0 -c 1 -m None --poll 10 --task_port=54829 --result_port=54396 --logdir=/gxfs_work1/fs1/work-geomar3/smomw122/2021-07_parsl_first_steps/2021.07.26.1_slurm_nesh/runinfo/000/NESH_01 --block_id=9 --hb_period=30  --hb_threshold=120 --cpu-affinity none 
